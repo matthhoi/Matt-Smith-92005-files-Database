b191f393a51e56e630e58c0a92daea720dee67fd
matthhoi <smithm4@stu.otc.school.nz>
2024-08-13 13:01:36 +1200

    Adding basic interface

--- 92005 - Python.py
+++ 92005 - Python.py
@@ -26,4 +26,35 @@ def all_pieces():
         print("All infomation of All Black players:\n")
         print(tabulate(results, headings,tablefmt="plain",colalign=alignments))
 
-
+if __name__ == "__main__":
+    count = "i"
+    option = "1"
+    while option != "5":
+        os.system("cls")
+        print("---------MENU---------")
+        print("1. ")
+        print("2. ")
+        print("3. ")
+        print("4. ")
+        print("5. Exit")
+        while count != "Exit":
+            option = input("choose an option from the menu 1, 2, 3, 4, and 5: ")
+            os.system("cls")
+            if option == "1":
+                all_pieces()
+                count = "Exit"
+            elif option == "2":
+                fly_half()
+                count = "Exit"
+            elif option == "3":
+                most_points()
+                count = "Exit"
+            elif option == "4":
+                started_after_2010_50_tests()
+                count = "Exit"
+            elif option == "5":
+                print("Goodbye.........")
+                exit()
+            else:
+                print("This is not one of the options try again")
+        count = input("Enter to return to menu...")
