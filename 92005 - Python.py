7a46b6bf677780db43c0815bf86e0df8a06e71d1
matthhoi <smithm4@stu.otc.school.nz>
2024-08-20 13:27:02 +1200

    Adding the user friendly gui

--- 92005 - Python.py
+++ 92005 - Python.py
@@ -9,23 +9,25 @@ LOWEST_PRICE = 25
 #import staments
 import sqlite3
 import os
-# run pip3 install tabulate to use this libry to print results in columns
+#run pip3 install tabulate to use this libry to print results in columns
 from tabulate import tabulate
+#run pip3 install easygui to use this libry to use the gui
+import easygui
 
 #funtions for the Querys
 def all_pieces():
     '''Deplay all infomation of all pieces in the Āhua_Gallery'''
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        qrl = "SELECT * FROM pieces;"
+        qrl = "SELECT piece_id, piece_name, price, material, maker FROM pieces;"
         cursor.execute(qrl)
         results = cursor.fetchall()
-        headings = ["piece_id","piece_name","price","purchase status",
-                    "material","commission", "maker"]
-        alignments = ("left","left","left","left","center","center","center")
+        headings = ["piece_id","piece_name","price","material", "maker"]
+        alignments = ("left","left","left","center","center")
         #print resalts of query
-        print("All infomation of the peices in the Āhua_Gallery:\n")
-        print(tabulate(results, headings,tablefmt="plain",colalign=alignments))
+        easygui.msgbox("All infomation of the peices in the Āhua_Gallery:\n" + 
+                       tabulate(results, headings,tablefmt="plain",
+                                colalign=alignments), "MENU")
 
 def all_sold():
     '''Deplay all usefull infomation of all pieces in the Āhua_Gallery that 
@@ -33,26 +35,27 @@ def all_sold():
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
         qrl = '''SELECT pieces.piece_id, pieces.price, pieces.piece_name, 
-        customers.first_name, customers.last_name, customers.postal_address, 
-        customers.town, customers.postal_code FROM pieces JOIN customer_piece, 
-        customers ON customer_piece.piece_id = pieces.piece_id AND 
-        customer_piece.customer_id = customers.customer_id;'''
+        customers.first_name, customers.postal_address 
+        FROM pieces JOIN customer_piece, customers ON customer_piece.piece_id
+        = pieces.piece_id AND customer_piece.customer_id = 
+        customers.customer_id;'''
         cursor.execute(qrl)
         results = cursor.fetchall()
         headings = ["piece_id","price","piece_name","first_name",
-                    "last_name","postal_address", "town", "postal_code"]
-        alignments = ("left","left","left","left","left","left","left","center")
+                    "last_name","postal_address"]
+        alignments = ("left","left","left","left","left")
         #print resalts of query
-        print("All usefull infomation of the peices in the Āhua_Gallery that "
-              "have sold:\n")
-        print(tabulate(results, headings,tablefmt="plain",colalign=alignments))
+        easygui.msgbox("All usefull infomation of the peices in the Āhua_Gallery"
+                       " that have sold:\n" + 
+                       tabulate(results, headings,tablefmt="plain",
+                                colalign=alignments), "MENU")
 
 def all_under_price():
-    '''Deplay all usefull infomation of all pieces in the Āhua_Gallery that 
-    have sold'''
+    '''Deplay all usefull infomation of the peices in the Āhua_Gallery that are 
+    under a certain price'''
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        price = int(input("Sort by what price?"))
+        price = int(easygui.enterbox("Sort by what price?", "MENU"))
         os.system("cls")
         qrl = f'''SELECT piece_id, piece_name, price, material, maker FROM 
         pieces WHERE price <= {price} AND purchase_status = 'Avalbile';'''
@@ -61,35 +64,31 @@ def all_under_price():
         headings = ["piece_id","piece_name","price","material","maker",]
         alignments = ("left","left","center","left","left")
         #print resalts of query
-        print("All usefull infomation of the peices in the Āhua_Gallery that "
-              "are under a certain price:\n")
-        print(tabulate(results, headings,tablefmt="plain",colalign=alignments))
+        easygui.msgbox("All usefull infomation of the peices in the Āhua_Gallery"
+                       " that are under a certain price:\n" + 
+                       tabulate(results, headings,tablefmt="plain",
+                                colalign=alignments), "MENU")
 
+#gui interface
 if __name__ == "__main__":
     count = "i"
     option = "1"
     while option != "5":
-        os.system("cls")
-        print("---------MENU---------")
-        print("1. ")
-        print("2. ")
-        print("3. ")
-        print("4. Exit")
+        possible_querys = ["1. Deplaying all infomation of pieces", "2. "
+                           "Deplaying all pieces that have sold", "3. Deplaying"
+                           " all peices that are under a certain price", "4. "
+                           "Exit"]
         while count != "Exit":
-            option = input("Choose an option from the menu 1, 2, 3, and 4: ")
-            os.system("cls")
-            if option == "1":
+            option = easygui.choicebox("---------MENU---------", "MENU", 
+                                       possible_querys)
+            if option == "1. Deplaying all infomation of pieces":
                 all_pieces()
-                count = "Exit"
-            elif option == "2":
+            elif option == "2. Deplaying all pieces that have sold":
                 all_sold()
-                count = "Exit"
-            elif option == "3":
+            elif option == "3. Deplaying all peices that are under a certain price":
                 all_under_price()
-                count = "Exit"
-            elif option == "4":
-                print("Goodbye.........")
+            elif option == "4. Exit":
+                easygui.msgbox("Goodbye.........", "MENU")
                 exit()
             else:
-                print("This is not one of the options try again")
-        count = input("Enter to return to menu...")
+                easygui.msgbox("This is not one of the options try again", "MENU")
