2cdd5c8b0097809d04c35d6dab748659df23c076
matthhoi <smithm4@stu.otc.school.nz>
2024-09-05 14:44:15 +1200

    Flowing conventions

--- 92005 - Python.py
+++ 92005 - Python.py
@@ -1,60 +1,64 @@
-'''A program to conect to the Āhua_Gallery database and run difrent queries on 
+"""A program to conect to the Āhua_Gallery database and run difrent queries on 
 it.
-By: Matt Smith                                                      6/9/2024'''
+By: Matt Smith                                                      6/9/2024"""
 
-#all the constences
+# all the constences
 DATABASE = "92005 - Database.SQlite"
 LOWEST_PRICE = 25
 
-#import staments
+# import staments
 import sqlite3
 import os
-#run pip3 install tabulate to use this libry to print results in columns
+# run pip3 install tabulate to use this libry to print results in columns
 from tabulate import tabulate
-#run pip3 install easygui to use this libry to use the gui
+# run pip3 install easygui to use this libry to use the gui
 import easygui
 
-#funtions for the Querys
+# funtions for the Querys
+
+
 def all_pieces():
-    '''Deplay all infomation of all pieces in the Āhua_Gallery'''
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
+    """Deplay all infomation of all pieces in the Āhua_Gallery"""
+    with sqlite3.connect(DATABASE) as d_b:
+        cursor = d_b.cursor()
         qrl = "SELECT piece_id, piece_name, price, material, maker FROM pieces;"
         cursor.execute(qrl)
         results = cursor.fetchall()
         headings = ["piece_id","piece_name","price","material", "maker"]
         alignments = ("left","left","left","center","center")
-        #print resalts of query
+        # print resalts of query
         easygui.msgbox("All infomation of the peices in the Āhua_Gallery:\n" + 
                        tabulate(results, headings,tablefmt="plain",
                                 colalign=alignments), "MENU")
 
+
 def all_sold():
-    '''Deplay all usefull infomation of all pieces in the Āhua_Gallery that 
-    have sold'''
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        qrl = '''SELECT pieces.piece_id, pieces.price, pieces.piece_name, 
+    """Deplay all usefull infomation of all pieces in the Āhua_Gallery that 
+    have sold"""
+    with sqlite3.connect(DATABASE) as d_b:
+        cursor = d_b.cursor()
+        qrl = """SELECT pieces.piece_id, pieces.price, pieces.piece_name, 
         customers.first_name, customers.postal_address 
         FROM pieces JOIN customer_piece, customers ON customer_piece.piece_id
         = pieces.piece_id AND customer_piece.customer_id = 
-        customers.customer_id;'''
+        customers.customer_id;"""
         cursor.execute(qrl)
         results = cursor.fetchall()
         headings = ["piece_id","price","piece_name","first_name",
                     "last_name","postal_address"]
         alignments = ("left","left","left","left","left")
-        #print resalts of query
-        easygui.msgbox("All usefull infomation of the peices in the Āhua_Gallery"
-                       " that have sold:\n" + 
+        # print resalts of query
+        easygui.msgbox("All usefull infomation of the peices in the "
+                       "Āhua_Gallery that have sold:\n" + 
                        tabulate(results, headings,tablefmt="plain",
                                 colalign=alignments), "MENU")
 
+
 def all_under_price():
-    '''Deplay all usefull infomation of the peices in the Āhua_Gallery that are 
-    under a certain price'''
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
+    """Deplay all usefull infomation of the peices in the Āhua_Gallery that are 
+    under a certain price"""
+    with sqlite3.connect(DATABASE) as d_b:
+        cursor = d_b.cursor()
         while True:
             try:
                 price = int(easygui.enterbox("Sort by what price?", "MENU"))
@@ -71,17 +75,18 @@ def all_under_price():
         results = cursor.fetchall()
         headings = ["piece_id","piece_name","price","material","maker",]
         alignments = ("left","left","center","left","left")
-        #print resalts of query
-        easygui.msgbox("All usefull infomation of the peices in the Āhua_Gallery"
-                       " that are under a certain price:\n" + 
+        # print resalts of query
+        easygui.msgbox("All usefull infomation of the peices in the "
+                       "Āhua_Gallery that are under a certain price:\n" + 
                        tabulate(results, headings,tablefmt="plain",
                                 colalign=alignments), "MENU")
 
+
 def specific_customers():
-    '''Deplay infomation on specific customers in the Āhua_Gallery'''
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        qrl = f'''SELECT first_name FROM customers;'''
+    """Deplay infomation on specific customers in the Āhua_Gallery"""
+    with sqlite3.connect(DATABASE) as d_b:
+        cursor = d_b.cursor()
+        qrl = """SELECT first_name FROM customers;"""
         cursor.execute(qrl)
         customers = cursor.fetchall()
         while True:
@@ -94,23 +99,24 @@ def specific_customers():
                 option = option.replace(")", "")
                 option = option.replace(",", "")
                 os.system("cls")
-                qrl = f'''SELECT first_name, last_name, postal_address, town, 
-                postal_code FROM customers WHERE first_name = '{option}';'''
+                qrl = f"""SELECT first_name, last_name, postal_address, town, 
+                postal_code FROM customers WHERE first_name = '{option}';"""
                 cursor.execute(qrl)
                 results = cursor.fetchall()
                 headings = ["first_name","last_name","postal_address","town",
                             "postal_code"]
                 alignments = ("left","left","center","left","left")
-                #print resalts of query
+                # print resalts of query
                 easygui.msgbox("infomation on specific customers in the "
-                            "Āhua_Gallery:\n" + 
-                            tabulate(results, headings, tablefmt="plain", 
+                               "Āhua_Gallery:\n" + 
+                               tabulate(results, headings, tablefmt="plain", 
                                         colalign=alignments), "MENU")
                 break
             except:
                 easygui.msgbox("Please choose one", "MENU")
 
-#gui interface
+
+# gui interface
 if __name__ == "__main__":
     count = "i"
     option = "1"
@@ -126,7 +132,8 @@ if __name__ == "__main__":
                 all_pieces()
             elif option == "2. Deplaying all pieces that have sold":
                 all_sold()
-            elif option == "3. Deplaying all peices that are under a certain price":
+            elif option == "3. Deplaying all peices that are under a certain\
+                            price":
                 all_under_price()
             elif option == "4. Deplay infomation on specific customers":
                 specific_customers()
